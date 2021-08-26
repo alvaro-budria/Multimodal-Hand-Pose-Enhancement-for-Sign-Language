590ab76f9af7671be10503c27429b5ad4bf96157
Alvaro <al.fran.budria@gmail.com>
2021-08-26 23:03:14 +0200

    revertin previous commit

--- train_gan.py
+++ train_gan.py
@@ -60,7 +60,7 @@ def main(args):
     torch.manual_seed(23456)
     torch.cuda.manual_seed(23456)
     ## DONE variables
-    with wandb.init(project="B2H-H2S", name=args.exp_name, settings=wandb.Settings(start_method='fork')):
+    with wandb.init(project="B2H-H2S", name=args.exp_name):
 
         ## load data from saved files
         data_tuple = load_data(args, rng)
