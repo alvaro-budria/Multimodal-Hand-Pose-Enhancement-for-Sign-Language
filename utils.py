4852aeedb58ed40d4d13f076415d7320b074580b
Alvaro <al.fran.budria@gmail.com>
2021-09-22 22:28:43 +0200

    lower vid feats batch size

--- utils.py
+++ utils.py
@@ -500,7 +500,7 @@ def obtain_vid_feats(kp_dir, key):
     ids = _join_ids(ids, clip_ids_vid)
     ids = sorted(ids)
     print("Obtained ids! Entering proc_vid.obtain_feats", flush=True)
-    size = 200
+    size = 10
     for subset in range(0, len(ids), size):
         hand_feats = proc_vid.obtain_feats(key, ids[subset:subset+size])
         save_binary(hand_feats, f"video_data/{key}_vid_feats_{subset}-{subset+size}.pkl")
