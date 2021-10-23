1a14bfdd7771701f528abb2dc1d387ed41275a2c
Alvaro <al.fran.budria@gmail.com>
2021-10-23 21:05:42 +0200

    add validation epoch script

---
+++ H2Sclassifier/Train_Test/val_epoch.py
@@ -0,0 +1,30 @@
+from hyperparameters import *
+import torch
+from torch.autograd import Variable
+import numpy as np
+from timeit import default_timer as timer
+
+
+def test_epoch(model, train_X, train_Y, loss_function, BATCH_SIZE, rng):
+    val_loss = []
+    predY = []
+    model.eval()
+    batchinds = np.arange(train_X.shape[0] // BATCH_SIZE)
+    rng.shuffle(batchinds)
+    with torch.no_grad():
+        for bii, bi in enumerate(batchinds):
+            ## setting batch data
+            idxStart = bi * BATCH_SIZE
+            inputData = train_X[idxStart:(idxStart + BATCH_SIZE), :, :]
+            outputGT = train_Y[idxStart:(idxStart + BATCH_SIZE), :, :]
+            inputData = Variable(torch.from_numpy(inputData)).to(device)
+            outputGT = Variable(torch.from_numpy(outputGT)).to(device)
+
+            # Forward pass.
+            y_ = model(inputData)
+            predY.append(y_)
+
+            # Compute loss
+            loss = loss_function(y_, outputGT)
+            val_loss.append(loss.item())
+    return val_loss, predY
