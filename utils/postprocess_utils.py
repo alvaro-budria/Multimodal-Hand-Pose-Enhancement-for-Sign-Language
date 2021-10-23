a91e32119eef4356e26e1c73aff95c574202e2f1
Alvaro <al.fran.budria@gmail.com>
2021-10-23 19:02:46 +0200

    vectorize rmv_clips_nan

--- utils/postprocess_utils.py
+++ utils/postprocess_utils.py
@@ -2,27 +2,43 @@ import numpy as np
 
 
 # removes those clips that contain at least one nan value
+# def rmv_clips_nan(X, Y=None, T=None):
+#     x, y, t = [], [], []
+#     for sample in range(X.shape[0]):
+#         if T is None and Y is None:
+#             if not np.isnan(X[sample,:,:]).any():
+#                 x.append(X[sample,:,:])
+#         if T is None:
+#             if not (np.isnan(X[sample,:,:]).any() | np.isnan(Y[sample,:,:]).any()):
+#                 x.append(X[sample,:,:])
+#                 y.append(Y[sample,:,:])
+#         else:
+#             if not (np.isnan(X[sample,:,:]).any() | np.isnan(Y[sample,:,:]).any() | np.isnan(T[sample,:]).any()):
+#                 x.append(X[sample,:,:])
+#                 y.append(Y[sample,:,:])
+#                 t.append(T[sample,:])
+#     x = np.array(x)
+#     if Y is not None:
+#         y = np.array(y)
+#     if T is not None:
+#         t = np.array(t)
+#     return x, y, t
+
+
+# removes those clips that contain at least one nan value
 def rmv_clips_nan(X, Y=None, T=None):
-    x, y, t = [], [], []
-    for sample in range(X.shape[0]):
-        if T is None and Y is None:
-            if not np.isnan(X[sample,:,:]).any():
-                x.append(X[sample,:,:])
-        if T is None:
-            if not (np.isnan(X[sample,:,:]).any() | np.isnan(Y[sample,:,:]).any()):
-                x.append(X[sample,:,:])
-                y.append(Y[sample,:,:])
-        else:
-            if not (np.isnan(X[sample,:,:]).any() | np.isnan(Y[sample,:,:]).any() | np.isnan(T[sample,:]).any()):
-                x.append(X[sample,:,:])
-                y.append(Y[sample,:,:])
-                t.append(T[sample,:])
-    x = np.array(x)
+    idx_nan = np.argwhere(np.isnan(X).any(axis=0)).squeeze().tolist()
     if Y is not None:
-        y = np.array(y)
+        idx_nan += np.argwhere(np.isnan(Y).any(axis=0)).squeeze().tolist()
     if T is not None:
-        t = np.array(t)
-    return x, y, t
+        idx_nan += np.argwhere(np.isnan(T).any(axis=0)).squeeze().tolist()
+    idx_nan = sorted(set(idx_nan))
+    X = np.delete(X, idx_nan, axis=0)
+    if Y is not None:
+        Y = np.delete(Y, idx_nan, axis=0)
+    if T is not None:
+        T = np.delete(T, idx_nan, axis=0)
+    return X, Y, T
 
 
 # given a list of arrays (each corresponding to a clip) with varying lengths,
