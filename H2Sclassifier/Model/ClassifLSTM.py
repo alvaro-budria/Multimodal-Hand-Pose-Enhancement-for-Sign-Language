2a3c6401ccbac8839c336d8bbe8842aabd37c28a
Alvaro <al.fran.budria@gmail.com>
2021-10-23 19:27:35 +0200

    rename model file

--- H2Sclassifier/Model/ClassifLSTM.py
+++ H2Sclassifier/Model/ClassifLSTM.py
@@ -1,22 +1,22 @@
 # Here we define an LSTM that regresses the depth
-import torch
 import torch.nn as nn
 
-class DepthLSTM(nn.Module):
+class ClassifLSTM(nn.Module):
 
-  def __init__(self, hidden_size, num_layers, seq_len, batch_size, num_joints, NUM_CLASSES):
+  def __init__(self, hidden_size, num_layers, seq_len, batch_size, num_joints, NUM_CLASSES, bidirectional=True):
     super().__init__()
     self.hidden_size = hidden_size
     self.num_layers = num_layers
     self.seq_len = seq_len
     self.batch_size = batch_size
     self.NUM_CLASSES = NUM_CLASSES
+    self.bidirectional = bidirectional
 
     # Define the LSTM layer
-    self.lstm = nn.LSTM(5*num_joints, hidden_size, num_layers, bias=True, batch_first = True)
+    self.lstm = nn.LSTM(5*num_joints, hidden_size, num_layers, bias=True, batch_first=True)
 
     # Define a Linear Layer to obtain the depth coordinate
-    self.Linear = nn.Linear(hidden_size, NUM_CLASSES, bias=True)
+    self.Linear = nn.Linear(hidden_size, NUM_CLASSES, bidirectional=self.bidirectional)
 
   def forward(self, seq, state=None):
     h, state = self.lstm(seq, state) # h.shape = [batch_size, seq_len, hidden_size]
