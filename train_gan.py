b1c7fcc6bd1e91cf712b11c9e54fee2a3c45b2cc
Alvaro <al.fran.budria@gmail.com>
2021-11-11 20:01:55 +0100

    shuffle at every epoch

--- train_gan.py
+++ train_gan.py
@@ -110,6 +110,13 @@ def main(args):
             else:
                 train_generator(args, rng, generator, discriminator, reg_criterion, gan_criterion, g_optimizer, train_X, train_Y, epoch, train_summary_writer, train_feats=train_feats)
                 currBestLoss, prev_save_epoch = val_generator(args, generator, discriminator, reg_criterion, g_optimizer, d_optimizer, g_scheduler, d_scheduler, val_X, val_Y, currBestLoss, prev_save_epoch, epoch, val_summary_writer, val_feats=val_feats)
+            # Data shuffle
+            I = np.arange(len(train_X))
+            rng.shuffle(I)
+            train_X = train_X[I]
+            train_Y = train_Y[I]
+            if args.require_text or args.require_image:
+                train_feats = train_feats[I]
 
     shutil.copyfile(lastCheckpoint, args.model_path + f"/lastCheckpoint_{args.exp_name}.pth")  #  name last checkpoint as "lastCheckpoint.pth"
 
