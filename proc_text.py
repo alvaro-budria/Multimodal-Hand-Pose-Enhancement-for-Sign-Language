5f5d376437700e4d48453703d8c8f4d8bf6a7d57
Alvaro <al.fran.budria@gmail.com>
2021-11-14 15:51:26 +0100

    debugging

--- proc_text.py
+++ proc_text.py
@@ -76,7 +76,7 @@ def obtain_embeddings(key, ids, method="BERT"):
         print(type(hidden_states), flush=True)
         for hidden_state in hidden_states:
             print("*********************************+", flush=True)
-            print('Tensor shape for each layer: ', hidden_state[0].shape, flush=True)
+            print('Tensor shape for each layer: ', hidden_state.shape, flush=True)
 
         # # Stores the token vectors, with shape [22 x 768]
         # token_vecs_sum = []
