9f4dd4a39ac9dd777c35f52012afa45c15adfced
Alvaro <al.fran.budria@gmail.com>
2021-11-20 18:54:21 +0100

    adding log of predictions

--- H2Sclassifier/Train_Test/val_epoch.py
+++ H2Sclassifier/Train_Test/val_epoch.py
@@ -13,6 +13,7 @@ def val_epoch(model, train_X, train_Y, loss_function, BATCH_SIZE, rng):
     rng.shuffle(batchinds)
     with torch.no_grad():
         for bii, bi in enumerate(batchinds):
+            print(f"bii: {bii}", flush=True)
             ## setting batch data
             idxStart = bi * BATCH_SIZE
             inputData = train_X[idxStart:(idxStart + BATCH_SIZE), :, :]
@@ -20,14 +21,14 @@ def val_epoch(model, train_X, train_Y, loss_function, BATCH_SIZE, rng):
             inputData = Variable(torch.from_numpy(inputData).float()).to(device)
             outputGT = Variable(torch.from_numpy(outputGT-1)).to(device)
             print(f"outputGT.shape {outputGT.shape}", flush=True)
-            GT.append(outputGT.cpu().numpy().tolist())
+            GT + outputGT.cpu().numpy().tolist()
             print(outputGT.cpu().numpy().tolist(), flush=True)
 
             # Forward pass
             y_, _ = model(inputData)
             print(f"y_.shape {y_.shape}", flush=True)
             print(f"np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1).shape {np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1).shape}", flush=True)
-            predY.append(np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1).tolist())
+            predY + np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1).tolist()
             print(np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1).tolist(), flush=True)
             epoch_acc += sum(np.argmax(y_[:,-1,:].cpu().detach().numpy(), axis=1) == outputGT.cpu().detach().numpy())
 
