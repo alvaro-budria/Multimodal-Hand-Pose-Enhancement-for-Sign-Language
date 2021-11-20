622942cca76bcdd503ee2b72c9b16f51024253b9
Alvaro <al.fran.budria@gmail.com>
2021-11-20 18:37:28 +0100

    adding log of predictions

--- H2Sclassifier/Train_Test/main.py
+++ H2Sclassifier/Train_Test/main.py
@@ -79,6 +79,8 @@ def main(args):
             print("Starting epoch: ", epoch, flush=True)
             train_epoch_loss, train_acc = train_epoch(model, X_train, Y_train, optimizer, loss_function, config.batch_size, rng)
             val_epoch_loss, val_acc, (GT, predY) = val_epoch(model, X_val, Y_val, loss_function, config.batch_size, rng)
+            print(f"len(GT), len(predY) {len(GT), len(predY)}", flush=True)
+
             wandb.log({"epoch": epoch,
                        "loss_train": np.mean(train_epoch_loss),
                        "loss_val": np.mean(val_epoch_loss),
