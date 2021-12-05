9709c50810693b29678d0026a902d9022fd46a38
Alvaro <al.fran.budria@gmail.com>
2021-12-05 15:26:17 +0100

    only make equal length if data does not have predetermined length

--- H2Sclassifier/Train_Test/main.py
+++ H2Sclassifier/Train_Test/main.py
@@ -129,7 +129,8 @@ def load_data(data_dir="../../video_data", data_type="r6d", key="train"):
          "groupedWordBert": f"True{key}_wordBert_embeddings.pkl",}
     X = load_binary(f"{data_dir}/{f[data_type]}")
     Y = load_binary(f"{data_dir}/categs_{key}.pkl")
-    X = make_equal_len(X, method="cutting+reflect", maxpad=192*(1 + 10*(data_type=="grouped_r6d")))  # make sequences have equal length, as initially they have different lengths
+    if data_type not in ["wordBert", "groupedWordBert"]:
+        X = make_equal_len(X, method="cutting+reflect", maxpad=192*(1 + 10*(data_type=="grouped_r6d")))  # make sequences have equal length, as initially they have different lengths
     X, Y, _ = rmv_clips_nan(X, Y)  # remove those clips containing nan values
     return X, Y
 
