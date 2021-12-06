a432414645f9bd87a20ce3c20c9b92f62e33d45b
Alvaro <al.fran.budria@gmail.com>
2021-12-06 11:03:28 +0100

    debug loss

--- H2Sclassifier/Train_Test/MLP_main.py
+++ H2Sclassifier/Train_Test/MLP_main.py
@@ -131,11 +131,14 @@ def train_epoch(model, train_X, train_Y, optimizer, loss_function, BATCH_SIZE, r
 
         # Forward pass
         y_ = model(inputData)
+        print(f"y_.shape {y_.shape}", flush=True)
+        print(f"outputGT.shape {outputGT.shape}", flush=True)
         epoch_acc += sum(np.argmax(y_[:,:].cpu().detach().numpy(), axis=1) == outputGT.cpu().detach().numpy())
 
         # Set gradients to 0, compute the loss, gradients, and update the parameters
         optimizer.zero_grad()
         loss = loss_function(y_[:,:], outputGT)
+        print(f"loss.item() {loss.item()}", flush=True)
         epoch_loss.append(loss.item())
         loss.backward()
         if clip_grad:
