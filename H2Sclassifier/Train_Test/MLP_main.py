fix train accuracy computation

H2Sclassifier/Train_Test/MLP_main.py
@@ -128,12 +128,18 @@ def train_epoch(model, train_X, train_Y, optimizer, loss_function, BATCH_SIZE, r
         outputGT = train_Y[idxStart:(idxStart + BATCH_SIZE)]
         inputData = Variable(torch.from_numpy(inputData).float()).to(device)
         outputGT = Variable(torch.from_numpy(outputGT-1)).to(device)
+        
+        GT = GT + outputGT.cpu().numpy().tolist()
 
         # Forward pass
         y_ = model(inputData)
-        y_ = torch.squeeze(y_)
-        
-        epoch_acc += sum(np.argmax(y_.cpu().detach().numpy()) == outputGT.cpu().detach().numpy())
+        predY = predY + np.argmax(y_.cpu().detach().numpy(), axis=1).tolist()
+        epoch_acc += sum(np.argmax(y_.cpu().detach().numpy(), axis=1) == outputGT.cpu().detach().numpy())
+
+        # Forward pass
+        # y_ = model(inputData)
+        # y_ = torch.squeeze(y_)
+        # epoch_acc += sum(np.argmax(y_.cpu().detach().numpy()) == outputGT.cpu().detach().numpy())
 
         # Set gradients to 0, compute the loss, gradients, and update the parameters
         optimizer.zero_grad()